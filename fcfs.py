a7bb259e8a91d81881bedb977975bbe4781468f4
kalilcazes <kalilcazes@gmail.com>
2019-08-27 22:39:08 -0300

    Commit inicial

---
+++ fcfs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.7
+
+import funcoes
+
+
+def executa(matriz):
+    #inicio, fim, prioridade = []
+    for processos in range(int(matriz[0])):
+        inicio[i] = funcoes.get_inicio(matriz, processos[i])
+        fim[i] = funcoes.get_fim(matriz, processos[i])
+        prioridade[i] = funcoes.get_prioridade(matriz, processos[i])
+
+
+        
---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.7
+
+""" import fcfs """
+import rr
+import funcoes
+""" import sjf
+import srtf
+import prioc
+import priop
+import priod """
+
+
+
+
+arq = open("entrada", "r").read()
+matriz = arq.split('\n')
+print(matriz)
+for i in range(1,len(matriz)):
+    matriz[i] = matriz[i].split(" ")
+
