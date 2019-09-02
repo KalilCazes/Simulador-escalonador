4c72517032565a71a3797e753a6d2e303495fcde
treinamento01 <brunonevesufrj@gmail.com>
2019-09-02 16:02:10 -0300

    Algoritmo PrioD

---
+++ priod.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3.7
+from tarefa import print_relatorio_tarefas
+
+
+def executa(lista_tarefas):
+    tempo_atual = 0
+    tarefas_prontas = [tarefa for tarefa in lista_tarefas if tarefa.chegada == tempo_atual]
+    tarefas_futuras = [tarefa for tarefa in lista_tarefas if tarefa.chegada > tempo_atual]
+    tarefas_terminadas = []
+
+    while len(tarefas_terminadas) != len(lista_tarefas):
+        if len(tarefas_prontas) == 0:
+            tempo_atual += 1
+
+            for tarefa_futura in tarefas_futuras:
+                if tarefa_futura.chegada == tempo_atual:
+                    tarefas_prontas.append(tarefa_futura)
+
+        else:
+            tarefa_executando = tarefas_prontas[0]
+
+            for t in tarefas_prontas:
+                if t.prioridade_dinamica > tarefa_executando.prioridade_dinamica:
+                    tarefa_executando = t
+
+            tarefa_executando.prioridade_dinamica = tarefa_executando.prioridade
+            tarefas_prontas.remove(tarefa_executando)
+            tarefa_executando.tempo_espera += tempo_atual - tarefa_executando.tempo_ultima_interrupcao
+
+            nova_maior_prioridade = False
+            while nova_maior_prioridade is False and tarefa_executando.tempo_restante > 0:
+                tarefa_executando.tempo_restante -= 1
+                tempo_atual += 1
+
+                # tarefa em execução ...
+
+                for tarefa_futura in tarefas_futuras:
+                    if tarefa_futura.chegada == tempo_atual:
+                        tarefas_prontas.append(tarefa_futura)
+
+                for tarefa_pronta in tarefas_prontas:
+                    tarefa_pronta.prioridade_dinamica += 1
+                    if tarefa_pronta.prioridade_dinamica > tarefa_executando.prioridade_dinamica:
+                        nova_maior_prioridade = True
+
+            if tarefa_executando.tempo_restante > 0:
+                tarefa_executando.tempo_ultima_interrupcao = tempo_atual
+                tarefas_prontas.append(tarefa_executando)
+
+            else:
+                tarefa_executando.tempo_total_execucao = tempo_atual - tarefa_executando.chegada
+                tarefas_terminadas.append(tarefa_executando)
+
+    print_relatorio_tarefas(lista_tarefas, "Prioridade Dinâmico")
