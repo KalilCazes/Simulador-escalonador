856a2b70ab8057d58b84cb77692d97823e3c51b9
treinamento01 <brunonevesufrj@gmail.com>
2019-09-02 15:56:21 -0300

    Adicionando campos necessários para entidade Tarefa

--- tarefa.py
+++ tarefa.py
@@ -1,10 +1,14 @@
 class Tarefa:
-    chegada = -1;
-    tempo_execucao = -1;
-    prioridade = -1;
+    chegada = -1
+    tempo_execucao = -1
+    prioridade = -1
+
     tempo_restante = -1
-    tempo_total_execucao = -1
+    tempo_total_execucao = 0
+    tempo_ultima_interrupcao = 0
     tempo_espera = 0
+
+    prioridade_dinamica = -1
 
     def __str__(self):
         return "tarefa [ chegada: " + str(self.chegada) \
@@ -15,14 +19,14 @@ class Tarefa:
                + ", tempo total de execução: " + str(self.tempo_total_execucao) + "]\n "
 
 
-def print_relatorio(tarefas, nome_algoritmo):
-    tempo_medio_espera = 0
-    tempo_medio_execucao = 0
+def print_relatorio_tarefas(tarefas, nome_algoritmo):
+    tempo_total_espera = 0
+    tempo_total_execucao = 0
     print("\n=====  " + nome_algoritmo + "  =====\n")
     for tarefa in tarefas:
         print(tarefa)
-        tempo_medio_execucao += tarefa.tempo_total_execucao
-        tempo_medio_espera += tarefa.tempo_espera
-    print("- tempo medio de execução = " + str(tempo_medio_execucao / len(tarefas)))
-    print("- tempo medio de espera = " + str(tempo_medio_espera / len(tarefas)))
+        tempo_total_execucao += tarefa.tempo_total_execucao
+        tempo_total_espera += tarefa.tempo_espera
+    print("- tempo medio de execução = " + str(tempo_total_execucao / len(tarefas)))
+    print("- tempo medio de espera = " + str(tempo_total_espera / len(tarefas)))
     print("\n======================================\n")
